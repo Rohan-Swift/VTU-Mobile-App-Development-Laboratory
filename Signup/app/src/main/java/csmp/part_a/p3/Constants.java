c2b6accd7637100e25bb419bda5505607970c029
Rohan Rajivnath <48855315+Rohan-Swift@users.noreply.github.com>
2021-05-19 02:46:46 +0530

    Delete Signup directory

--- Signup/app/src/main/java/csmp/part_a/p3/Constants.java
+++
@@ -1,7 +0,0 @@
-package csmp.part_a.p3;
-
-public class Constants {
-    public static final String USERNAME_KEY = "userNameKey";
-    public static final String PASSWORD_KEY = "passwordKey";
-    
-}
